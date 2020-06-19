74b8354a25275a23711e2c6631586cafc5c96f71
Kioko <kllk.spb@mail.ru>
2020-06-19 17:06:50 +0300

    Лабораторная 3

---
+++ 3 laba/Location.java
@@ -0,0 +1,29 @@
+import java.util.Objects;
+/*This class represents a specific location in a 2D map.
+  Coordinates are integer values.*/
+public class Location {
+    //X coordinate of this location.
+    public int xCoord;
+    //Y coordinate of this location.
+    public int yCoord;
+    //Creates a new location with the specified integer coordinates.
+    public Location(int x, int y) {
+        xCoord = x;
+        yCoord = y;
+    }
+    //Creates a new location with coordinates (0, 0).
+    public Location() {
+        this(0, 0);
+    }
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        Location location = (Location) obj;
+        return xCoord == location.xCoord && yCoord == location.yCoord;
+    }
+    public int hashCode() {
+        return Objects.hash(xCoord, yCoord);
+    }
+}
